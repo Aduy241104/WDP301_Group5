f838e609be808c1baef44233c24b6cf48baa6313
Qthai25 <thaivo307@gmail.com>
2026-01-22 14:27:31 +0700

    Done View Order List, Filter Orders by Status, Search Orders, View Order Detail, Confirm / Cancel Order, Update Order Status

--- Backend/src/router/index.js
+++ Backend/src/router/index.js
@@ -2,6 +2,7 @@ import userRoute from "./user.routes.js";
 import otpCodeRoute from "./otpCode.routes.js";
 import authenticationRoute from "./authentication.routes.js";
 import productDiscoveryRoute from "./productDiscovery.routes.js";
+import sellerOrderRouter from "./sellerOrder.route.js";
 
 
 function route(app) {
@@ -9,6 +10,7 @@ function route(app) {
     app.use("/api/user", userRoute);
     app.use("/api/otp", otpCodeRoute);
     app.use("/api/discovery", productDiscoveryRoute);
+    app.use("/api/seller/orders", sellerOrderRouter);
 }
 
 export default route;
