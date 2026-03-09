277562c01c6614bc7857d116594b89c2ced38be0
jikanCe18 <kimchikhang921@gmail.com>
2026-03-09 10:59:16 +0700

    fix Banner

--- Backend/src/controllers/AdminCategoryController.js
+++ Backend/src/controllers/AdminCategoryController.js
@@ -1,25 +1,27 @@
 import { StatusCodes } from "http-status-codes";
-import { ShopCategory } from "../models/ShopCategory.js";
+import { CategorySchema } from "../models/CategorySchema.js";
 
 /**
  * GET /api/admin/categories
- * Admin lấy danh sách category để dùng cho form chón
+ * Admin lấy danh sách category để dùng cho form chọn
  */
 export const getAdminCategories = async (req, res) => {
-    try {
+  try {
 
-        const categories = await ShopCategory.find()
-            .select("_id name")
-            .sort({ name: 1 });
+    const categories = await CategorySchema.find({
+      isDeleted: false,
+      isActive: true
+    })
+      .select("_id name")
+      .sort({ name: 1 });
 
-        return res.status(StatusCodes.OK).json(categories);
+    return res.status(StatusCodes.OK).json(categories);
 
-    } catch (error) {
+  } catch (error) {
 
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            message: error.message
-        });
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: error.message
+    });
 
-    }
+  }
 };
-
